7667e2de85e06957678f1a1e75673f84e8ca6309
bottlewook <bwlimtony@gmail.com>
2024-06-10 23:09:56 +0900

    feat: authenticatedWithEmailAndPassword 함수 제작하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersModel } from 'src/users/entities/users.entity';
 import { JWT_SECRET } from './const/auth.const';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly JwtServices: JwtService) { }
+  constructor(
+    private readonly JwtServices: JwtService,
+    private readonly usersService: UsersService,
+  ) { }
   /**
    * 우리가 만드려는 기능
    *
@@ -43,5 +48,30 @@ export class AuthService {
       secret: JWT_SECRET,
       expiresIn: isRefreshToken ? 3600 : 300,
     });
+  }
+
+  loginUser(user: Pick<UsersModel, 'email' | 'id'>) {
+    return {
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
+    };
+  }
+
+  async authenticatedWithEmailAndPassword(
+    user: Pick<UsersModel, 'email' | 'password'>,
+  ) {
+    const existingUser = await this.usersService.getUserByEmail(user.email);
+
+    if (!existingUser) {
+      throw new UnauthorizedException('존재하지 않는 사용자입니다.');
+    }
+
+    const passOk = await bcrypt.compare(user.password, existingUser.password);
+
+    if (!passOk) {
+      throw new UnauthorizedException('비밀번호가 틀렸습니다.');
+    }
+
+    return existingUser;
   }
 }
