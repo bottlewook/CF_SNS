2c816b6139ad20b83785c88e61f98da4c955994d
bottlewook <bwlimtony@gmail.com>
2024-06-10 23:17:54 +0900

    feat: loginWithEmail 함수 작업

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -74,4 +74,10 @@ export class AuthService {
 
     return existingUser;
   }
+
+  async loginWithEmail(user: Pick<UsersModel, 'email' | 'password'>) {
+    const existingUser = await this.authenticatedWithEmailAndPassword(user);
+
+    return this.loginUser(existingUser);
+  }
 }
