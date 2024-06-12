2305d80e914c9148f92bb0fb4b00ae022af5fbb7
bottlewook <bwlimtony@gmail.com>
2024-06-12 23:17:35 +0900

    feat: 토큰 재발급 로직 완성하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -68,6 +68,31 @@ export class AuthService {
     };
   }
 
+  verifyToken(token: string) {
+    return this.JwtServices.verify(token, {
+      secret: JWT_SECRET,
+    });
+  }
+
+  rotateToken(token: string, isRefreshToken: boolean) {
+    const decoded = this.JwtServices.verify(token, {
+      secret: JWT_SECRET,
+    });
+
+    if (decoded.type !== 'refresh') {
+      throw new UnauthorizedException(
+        '토큰 재발급은 Refresh 토큰으로만 가능합니다!',
+      );
+    }
+
+    return this.signToken(
+      {
+        ...decoded,
+      },
+      isRefreshToken,
+    );
+  }
+
   signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
     const payload = {
       email: user.email,
