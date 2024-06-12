79dd7a93b3838844d81c98d81541a967233a29e1
bottlewook <bwlimtony@gmail.com>
2024-06-12 21:45:05 +0900

    feat: registerWithEmail 함수 작성하기

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersModel } from './entities/users.entity';
 import { Repository } from 'typeorm';
@@ -10,14 +10,34 @@ export class UsersService {
     private readonly usersRepository: Repository<UsersModel>,
   ) { }
 
-  async createUser(nickname: string, email: string, password: string) {
-    const user = this.usersRepository.create({
-      nickname,
-      email,
-      password,
+  async createUser(users: Pick<UsersModel, 'nickname' | 'email' | 'password'>) {
+    const nicknameExists = await this.usersRepository.exists({
+      where: {
+        nickname: users.email,
+      },
     });
 
-    const newUser = await this.usersRepository.save(user);
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname입니다!');
+    }
+
+    const emailExists = await this.usersRepository.exists({
+      where: {
+        email: users.email,
+      },
+    });
+
+    if (emailExists) {
+      throw new BadRequestException('이미 존재하는 email입니다!');
+    }
+
+    const userObject = this.usersRepository.create({
+      nickname: users.nickname,
+      email: users.email,
+      password: users.password,
+    });
+
+    const newUser = await this.usersRepository.save(userObject);
 
     return newUser;
   }
